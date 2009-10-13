40739c0b8c19c920967abbac11ffa67cd9ece804
stig.lau <stig@lau.no>
2009-10-13 22:12:23 +0200

    Tests improved considerably

--- src/test/scala/no/lau/domain/CascadingMovementTest.scala
+++ src/test/scala/no/lau/domain/CascadingMovementTest.scala
@@ -5,16 +5,15 @@ import org.junit.Test
 
 class CascadingMovementTest {
   val game = new Game(3, 2) {
-    gameBoard += (0, 1) -> StaticWall()
     gameBoard += (1, 1) -> Block(this, "a")
     gameBoard += (2, 1) -> Block(this, "b")
   }
 
-  val leif = new Monster(game, "MonsterLeif") {
-    game.gameBoard += (1, 0) -> this
-  }
+  @Test def monsterMovingBlocksTest() {
+    val leif = new Monster(game, "MonsterLeif") {
+      game.gameBoard += (1, 0) -> this
+    }
 
-  @Test def cascadingMovementTest() {
     game printBoard ()
     leif move (Up)
     assertEquals((1, 1), game.whereIs(leif))
@@ -29,7 +28,19 @@ class CascadingMovementTest {
     leif move (Left)
     game printBoard ()
     assertEquals((1, 2), game.whereIs(leif))
-    assertEquals((0, 1), game.whereIs(StaticWall()))
     assertEquals((0, 2), game.whereIs(Block(game, "a")))
+  }
+
+  @Test def cascadingMovementTest() {
+    val leif = new Monster(game, "MonsterLeif") {
+      game.gameBoard += (0, 1) -> this
+    }
+
+    game printBoard ()
+    leif move (Right)
+    game printBoard ()
+    assertEquals(leif, game.gameBoard(1, 1))
+    assertEquals(Block(game, "a"), game.gameBoard(2, 1))
+    assertEquals(Block(game, "b"), game.gameBoard(3, 1))
   }
 }
