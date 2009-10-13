b820e04c677f62fa3e873c128d9eb0516ed97f71
stig.lau <stig@lau.no>
2009-10-13 22:01:06 +0200

    Cascading movement successfully moved to an own Test file

--- src/test/scala/no/lau/domain/CascadingMovementTest.scala
+++ src/test/scala/no/lau/domain/CascadingMovementTest.scala
@@ -1,26 +1,33 @@
 import no.lau.domain._
+import Direction.{Up, Down, Left, Right}
+import org.junit.Assert._
+import org.junit.Test
 
-object CascadingMovementTest {
-  def main(args: Array[String]) {
-    val game1 = new Game(3, 2) {
-      gameBoard += (0, 1) -> new StaticWall()
-      gameBoard += (1, 1) -> Block(this, "a")
-      gameBoard += (2, 1) -> Block(this, "b")
-    }
+class CascadingMovementTest {
+  val game = new Game(3, 2) {
+    gameBoard += (0, 1) -> StaticWall()
+    gameBoard += (1, 1) -> Block(this, "a")
+    gameBoard += (2, 1) -> Block(this, "b")
+  }
 
-    val leif = new Monster(game1, "MonsterLeif") {
-      game.gameBoard += (1, 0) -> this
-    }
+  val leif = new Monster(game, "MonsterLeif") {
+    game.gameBoard += (1, 0) -> this
+  }
 
-    import Direction.{Up, Down, Left, Right}
-    game1 printBoard ()
+  @Test def cascadingMovementTest() {
+    game printBoard ()
     leif move (Up)
-    game1 printBoard ()
+    game printBoard ()
     leif move (Right)
-    game1 printBoard ()
+    game printBoard ()
     leif move (Up)
-    game1 printBoard ()
+    game printBoard ()
     leif move (Left)
-    game1 printBoard ()
+    game printBoard ()
+    assertEquals((1, 2), game.whereIs(leif))
+    assertEquals((0, 1), game.whereIs(StaticWall()))
+
+    assertEquals((0, 2), game.whereIs(Block(game, "a")))
+    assertEquals((3, 1), game.whereIs(Block(game, "b")))
   }
 }
