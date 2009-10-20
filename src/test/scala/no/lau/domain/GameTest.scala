2e5259f03317da272e57ad1ecf0ea32a28849c9f
Stig Lau <stig@lau.no>
2009-10-20 20:30:09 +0200

    Implemented with actors for asyncronicity :)

--- src/test/scala/no/lau/domain/GameTest.scala
+++ src/test/scala/no/lau/domain/GameTest.scala
@@ -2,6 +2,7 @@ package no.lau.domain
 
 import java.io.{InputStreamReader}
 import no.lau.domain.movement._
+import actors.Actor
 
 /**
  * Test used for setting up and testing that the game holds together. 
@@ -12,20 +13,46 @@ object GameTest {
     val game = Game(20, 10)
     val rnd = new scala.util.Random
 
-    1 to 100 foreach {arg => game.addRandomly(Block(game, "a" + rnd.nextInt()))}
+    1 to 20 foreach {arg => game.addRandomly(Block(game, "block" + rnd.nextInt()))}
+    1 to 2 foreach {arg => game.addRandomly(Monster(game, "monster" + rnd.nextInt()))}
 
 
     val monsterGunnar = new Monster(game, "MonsterGunnar")
     game.addRandomly(monsterGunnar)
+    val directionHub = new AsymmetricGamingInterface(game, monsterGunnar)
+    directionHub.start
+    new KeyboardHandler(directionHub).start
 
+    println(game printableBoard)
+  }
+}
 
+class AsymmetricGamingInterface(game: Game, movable: Movable) extends Actor {
+  def act() {
+    while (true) {
+      try {
+        receive {
+          case direction: Direction => {
+            movable.move(direction)
+            game.newTurn
+            println(game printableBoard)
+          }
+          case _ => println("No direction")
+        }
+      } catch {
+        case ime: IllegalMoveException => println("Illegal Move!! " + ime.getMessage)
+      }
+    }
 
-    //This is the walking around part
+  }
+}
+
+class KeyboardHandler(gamingInterface: AsymmetricGamingInterface) extends Actor {
+  def act() {
     var continue = true
     while (continue) {
-      println(game printableBoard)
       val reader = new InputStreamReader(System.in)
-      val value = reader.read match {
+      val keyInput = reader.read match {
         case 'w' => Up
         case 'a' => Left
         case 's' => Down
@@ -33,17 +60,7 @@ object GameTest {
         case 'q' => continue = false
         case _ => println("Instructions: Use w, a, s, d keys to move. Enter to accept. q to exit")
       }
-      try {
-        value match {
-          case direction: Direction => {
-            monsterGunnar.move(direction)
-            game.newTurn
-          }
-          case _ => println("No direction")
-        }
-      } catch {
-        case ime: IllegalMoveException => println("Illegal Move!! " + ime.getMessage)
-      }
+      if (keyInput.isInstanceOf[Direction]) gamingInterface ! keyInput
     }
     println("Game ended")
   }
