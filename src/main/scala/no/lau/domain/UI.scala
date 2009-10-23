e0fc88a05aca75fc4e6639280880c76cd7b1e703
Stig Lau <stig@lau.no>
2009-10-23 14:56:59 +0200

    Updated UI codebase

--- src/main/scala/no/lau/domain/UI.scala
+++ src/main/scala/no/lau/domain/UI.scala
@@ -2,10 +2,10 @@ package no.lau.domain
 
 import no.lau.domain.movement._
 
-import javax.swing.{JComponent, KeyStroke}
 import java.awt.Font
 
 import scala.swing._
+import javax.swing.{InputMap, SwingUtilities, JComponent, KeyStroke, ActionMap}
 
 /**
  * Created by IntelliJ IDEA.
@@ -20,37 +20,49 @@ object UI extends SimpleGUIApplication {
 
    val monsterGunnar = new Monster(game, "MonsterGunnar")
    game.addRandomly(monsterGunnar)
-   val board = new TextArea(){
+   val gameBoard = new TextArea(){
      editable = false
      text = game printableBoard
-     
-     val inputMap = peer.getInputMap(JComponent.WHEN_FOCUSED)
-     inputMap.put(KeyStroke.getKeyStroke("UP"), "up")
-     inputMap.put(KeyStroke.getKeyStroke("DOWN"), "down")
-     inputMap.put(KeyStroke.getKeyStroke("LEFT"), "left")
-     inputMap.put(KeyStroke.getKeyStroke("RIGHT"), "right")
-     inputMap.put(KeyStroke.getKeyStroke("typed w"), "up")
-     inputMap.put(KeyStroke.getKeyStroke("typed s"), "down")
-     inputMap.put(KeyStroke.getKeyStroke("typed a"), "left")
-     inputMap.put(KeyStroke.getKeyStroke("typed d"), "right")
-     
-     val actionMap = peer.getActionMap()
-     actionMap.put("up", Action("KeyPressed"){monsterGunnar.move(Up); text = game printableBoard}.peer)
-     actionMap.put("down", Action("KeyPressed"){monsterGunnar.move(Down); text = game printableBoard}.peer)
-     actionMap.put("left", Action("KeyPressed"){monsterGunnar.move(Left); text = game printableBoard}.peer)
-     actionMap.put("right", Action("KeyPressed"){monsterGunnar.move(Right); text = game printableBoard}.peer)
-     
+
+     populateInputMap(peer.getInputMap(JComponent.WHEN_FOCUSED))
+     populateActionMap(monsterGunnar, peer.getActionMap())
      peer.setFont(new Font("Monospaced", peer.getFont().getStyle(), 24));
-     
    }
-   
+
    def top = new MainFrame {
      contents = new BorderPanel {
-       import BorderPanel.Position._
-       add(board, Center)
+       add(gameBoard, BorderPanel.Position.Center)
      }
    }
-   
-   def refresh = { board text = game printableBoard }
+
+
+  def populateInputMap(inputMap: InputMap) {
+    inputMap.put(KeyStroke.getKeyStroke("UP"), "up")
+    inputMap.put(KeyStroke.getKeyStroke("DOWN"), "down")
+    inputMap.put(KeyStroke.getKeyStroke("LEFT"), "left")
+    inputMap.put(KeyStroke.getKeyStroke("RIGHT"), "right")
+    inputMap.put(KeyStroke.getKeyStroke("typed w"), "up")
+    inputMap.put(KeyStroke.getKeyStroke("typed s"), "down")
+    inputMap.put(KeyStroke.getKeyStroke("typed a"), "left")
+    inputMap.put(KeyStroke.getKeyStroke("typed d"), "right")
+  }
+
+  def populateActionMap(movable:Movable, actionMap: ActionMap) {
+    actionMap.put("up", Action("KeyPressed") {move(movable, Up)}.peer)
+    actionMap.put("down", Action("KeyPressed") {move(movable, Down)}.peer)
+    actionMap.put("left", Action("KeyPressed") {move(movable, Left)}.peer)
+    actionMap.put("right", Action("KeyPressed") {move(movable, Right)}.peer)
+  }
+
+  def move(movable: Movable, direction: Direction) {
+    movable.move(direction)
+    gameBoard.text = game printableBoard
+  }
+
+  override def main(args: Array[String]) = {
+    SwingUtilities.invokeLater {
+      new Runnable {def run() {init(); top.pack(); top.visible = true}}
+    }
+  }
 }
 
