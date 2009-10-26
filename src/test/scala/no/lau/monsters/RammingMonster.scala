db04d1786786feed35b4f2cbcede9722f3d3f5f5
Stig Lau <stig@lau.no>
2009-10-26 10:08:35 +0100

    Ramming monster should use stackable movement :)

--- src/test/scala/no/lau/monsters/RammingMonster.scala
+++ src/test/scala/no/lau/monsters/RammingMonster.scala
@@ -1,12 +1,12 @@
 package no.lau.monsters
 
 import no.lau.domain.{Game, Monster}
-import no.lau.domain.movement.{Up, Movable}
 import no.lau.movement.TickListener
+import no.lau.domain.movement.{StackableMovement, Up, Movable}
 
-class RammingMonster(game: Game, id: Any) extends Monster(game, id) with Movable with TickListener {
+class RammingMonster(game: Game, id: Any) extends Monster(game, id) with StackableMovement with TickListener {
   override def tick {
-    move(Up)
+    stackMovement(Up)
     println("Moving up by tick")
   }
 }
