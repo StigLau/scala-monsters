fc5415e1aa001eb2db955ffe6cd5faec6b51884d
Stig Lau <stig@lau.no>
2009-11-01 17:38:59 +0100

    First example of what levels can look like - Stashing changes

---
+++ src/main/scala/no/lau/predefined/Levels.scala
@@ -0,0 +1,42 @@
+package no.lau.predefined
+
+import org.junit.Test
+
+/**
+ * Settings of each level according to the original Beasts Game
+ */
+case class Levels(
+        pullBlocks:Boolean,
+        gameSpeedUp:Boolean,
+        explosiveBlocks:Boolean,
+        superBeast:Range,
+        ratioEggs:Int,
+        hatchingSpeed:Int,
+        startingBeasts:Int)
+
+object LevelEasyA {
+  val level = Levels(true, false, false, 0 to 0, 0,	0, 1)
+}
+
+object LevelEasyB {
+  val level = Levels(true, false, false, 0 to 0, 0,	0, 2)
+}
+
+object LevelEasyC {
+  val level = Levels(true, false, false, 0 to 0, 0,	0, 3)
+}
+
+/*
+class TestLevels {
+  @Test
+  def testRunningLevels {
+    val game = Game(40, 25)
+   val rnd = new scala.util.Random
+   1 to 1 foreach {arg => game.addRandomly(Block(game, "a" + rnd.nextInt()))}
+   //1 to 10 foreach {arg => game.addRandomly(Monster(game, "monster" + rnd.nextInt()))}
+
+   val monsterGunnar = new Monster(game, "MonsterGunnar") with StackableMovement with Mortal with Pusher {
+     override def toString = ""
+   }
+  }
+} */
