6664c2dfef3d3142eef028bd6aeb1b659f1d72e1
Stig Lau <stiglau@McFeasty.local>
2010-09-28 12:25:35 +0200

    Project interoperable with sbt and Maven - Hooray

--- project/build/ScalaMonstersProject.scala
+++ project/build/ScalaMonstersProject.scala
@@ -1,13 +1,11 @@
-/**
- * Copyright (C) 2010, Mikko Peltonen, Jon-Anders Teigen
- * Licensed under the new BSD License.
- * See the LICENSE file for details.
- */
-
 import sbt._
+//import fi.jawsy.sbtplugins.jrebel.JRebelWebPlugin
 
-class ScalaMonstersProject(info:ProjectInfo) extends ParentProject(info) {
+class ScalaMonstersProject(info:ProjectInfo) extends ParentProject(info) //with JRebelWebPlugin
+{
 //  override def managedStyle = ManagedStyle.Maven
+  val mavenLocal = "Local Maven Repository" at "file://"+Path.userHome+"/.m2/repository"
+
 
   lazy val core = project("core", "scala-monsters-core")
   lazy val simpleGui = project("simple-gui", "scala-monsters-simple-gui", core)
